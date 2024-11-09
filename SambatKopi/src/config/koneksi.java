639803827c3542ffc38a7d317d98a846e033ffee
WAHYUAKHMADFADILLAH <wahyufadil1140@gmail.com>
2024-11-10 03:02:57 +0700

    clean

---
+++ SambatKopi/src/config/koneksi.java
@@ -0,0 +1,32 @@
+
+package config;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class koneksi {
+    public static Connection Go(){
+        try {
+//            MysqlDataSource m = new MysqlDataSource();
+//            String url = "jdbc:mysql://localhost:3306/sambaturip_db";
+//            String user = "root";
+//            String pass = "vipas";
+            MysqlDataSource m = new MysqlDataSource();
+            String url = "jdbc:mysql://localhost:3306/sambaturip_db";
+            String user = "root";
+            String pass = "Wahyu0085!";
+            
+            Connection C = DriverManager.getConnection(url, user, pass);
+            System.out.println("Koneksi sukses");
+            return C;
+        } catch (SQLException e) {
+            System.err.println("Koneksi Gagal!\n"+
+                    e.getMessage());
+        }
+        
+        
+        return null;
+    }
+}
---
+++ SambatKopi/src/main/KasirPage.java
@@ -0,0 +1,94 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author ADVAN
+ */
+public class KasirPage extends javax.swing.JFrame {
+
+    Profile p;
+    
+    public KasirPage() {
+        initComponents();
+    }
+    
+    KasirPage(Profile P) {
+        initComponents();
+        
+        this.p = P;
+        labelUser.setText(p.getFullname()+"("+p.getLevel()+")");
+    }
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        labelUser = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        labelUser.setText("jLabel1");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(181, 181, 181)
+                .addComponent(labelUser)
+                .addContainerGap(182, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(63, 63, 63)
+                .addComponent(labelUser)
+                .addContainerGap(221, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(KasirPage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(KasirPage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(KasirPage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(KasirPage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new KasirPage().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel labelUser;
+    // End of variables declaration//GEN-END:variables
+}
