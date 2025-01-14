f7a49dd87a1b801e58990d83c850e4a33bc118a1
WAHYUAKHMADFADILLAH <wahyufadil1140@gmail.com>
2025-01-14 23:15:31 +0700

    fix doc

--- SambatKopi/src/config/koneksi.java
+++ SambatKopi/src/config/koneksi.java
@@ -10,9 +10,9 @@ public class koneksi {
     public static Connection Go(){
         try {
             MysqlDataSource m = new MysqlDataSource();
-            String url = "jdbc:mysql://localhost:3306/sambat"; // v
+            String url = "jdbc:mysql://localhost:8889/sambat"; // 3306 for mysql default port
             String user = "root";
-            String pass = "vipas";
+            String pass = "root";
             
             Connection C = DriverManager.getConnection(url, user, pass);
             System.out.println("Koneksi sukses");
